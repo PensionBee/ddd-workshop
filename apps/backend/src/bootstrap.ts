cfb48ce4da8ec16ff9d089fc8c60e9783f4843fc
Craig McCallum <craig@pensionbee.com>
2023-07-28 15:45:43 +0100

    App starts properly

--- apps/backend/src/bootstrap.ts
+++ apps/backend/src/bootstrap.ts
@@ -5,48 +5,75 @@ import morgan from "morgan";
 import passport from "passport";
 import { Strategy } from "passport-local"; // basic auth to look up username and password
 import session from "express-session";
+import prisma from "#shared/infra/prisma";
 
 const createExpressApp = async () => {
   const app = express();
 
+  // Cookie parsing
   app.use(cookieParser());
 
+  // JSON body parsing
   app.use(express.json());
 
-  app.use(session({ secret: "secret" }));
+  // Session
+  app.use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: false },
+    })
+  );
 
+  // CORS config
   app.use(
     cors({
       origin: "*",
     })
   );
 
+  // Logging
   app.use(morgan("dev"));
 
+  // Authentication
   app.use(passport.initialize());
   app.use(passport.session());
-
   passport.use(
     new Strategy(
       { usernameField: "email", passwordField: "password" },
-      async function (email, password, done) {
-        // const user = await db.User.findOne({ where: { email: email } })
-        // FIXME: replace with user actually retrieved from DB
-        const user = { email: "foo", password: "bar" };
+      async (email, password, done) => {
+        const user = await prisma.users.findUnique({
+          where: {
+            email,
+          },
+        });
         if (!user) {
           return done(null, false);
         }
 
         const passwordIsValid = password === user.password;
-
         return passwordIsValid === true ? done(null, user) : done(null, false);
       }
     )
   );
 
-  app.use("/", (_req, res) => res.status(200).json({ test: "test" }));
+  // API routes
+  app.use("/api/", (_req, res) => res.status(200));
+  app.use("/*", (req, res) =>
+    res.status(404).json({
+      error: `${req.path} not found`,
+    })
+  );
 
+  // Server startup
   app.listen(3000, () => {
     console.log(`App listening on port ${3000}`);
   });
 };
+
+try {
+  createExpressApp();
+} catch (err) {
+  console.error(err);
+}
