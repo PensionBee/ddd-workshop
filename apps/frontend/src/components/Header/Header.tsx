9ed6875819488e631ea88e0138dabe6cd0154670
Toby Gates <tobygates@pensionbee.com>
2023-08-01 17:03:42 +0100

    Add some responsive styling

--- apps/frontend/src/components/Header/Header.tsx
+++ apps/frontend/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { GridContainer, GridItem, GridRow } from "@shared/components/Grid";
 import { MainNav } from "../../shared/components/Nav/MainNav/MainNav";
 import { Button } from "../../shared/components/Button";
 import { StyledHeader } from "./Header.styled";
+import { useMediaQueryMd } from "../../shared/hooks/useMediaQuery";
 
 const mainMenu = [
   {
@@ -19,15 +20,18 @@ const mainMenu = [
 ];
 
 const Header = () => {
+  const isMd = useMediaQueryMd();
   return (
     <StyledHeader>
       <GridContainer>
         <GridRow pb={8} pt={8} align="center">
-          <GridItem span={{ md: 4 }}>[Logo]</GridItem>
-          <GridItem span={{ md: 4 }} align="center">
-            <MainNav list={mainMenu} />
-          </GridItem>
-          <GridItem span={{ md: 4 }} align="end">
+          <GridItem span={{ xs: 6, md: 3 }}>[Logo]</GridItem>
+          {isMd ? (
+            <GridItem span={{ md: 6 }} align="center">
+              <MainNav list={mainMenu} />
+            </GridItem>
+          ) : null}
+          <GridItem span={{ xs: 6, md: 3 }} align="end">
             <Button onClick={() => alert("Clicked")}>Log In</Button>
           </GridItem>
         </GridRow>
