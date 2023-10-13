88d74aa3639430a789105fb570bf2d0111302bf6
Craig McCallum <craig@pensionbee.com>
2023-10-13 11:52:54 +0100

    Add account tests

---
+++ src/contexts/posts/core/entities/post.spec.ts
@@ -0,0 +1,6 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("parseAccount", () => {
+  test("It returns a valid Account entity if the data to parse is valid", () => {});
+  test("It throws an error if required data is missing", () => {});
+});
