8169d3616868b8ed072f4513f94c74412edeb860
Craig McCallum <craig@pensionbee.com>
2023-11-06 22:29:04 +0000

    Update ts problems

--- problems/set1.ts
+++ problems/set1.ts
@@ -1,5 +1,5 @@
 /**
- * Part 1: Change the types in the following code from 'any' to a more specific type
+ * Part 1: Change the types in the following code from 'any' to something more specific.
  *
  * Hint: Search for 'TS basic types'
  */
@@ -10,14 +10,13 @@ const isVerified: any = true;
 const favouriteTopics: any = ["World Politics", "Sports", "The Environment"];
 
 /**
- * Part 2: Change the types in the following code from 'any' to a more specific object type
+ * Part 2: Change the types in the following code from 'any' to something more specific.
  *
  * Hint: Search for 'TS object types'
  * Hint: Search for 'TS unions'
  */
 
 export type User = any;
-
 export const user1: User = {
   id: 1,
   email: "user1@test.com",
@@ -25,7 +24,6 @@ export const user1: User = {
   isVerified: true,
   favouriteTopics: ["World Politics", "Sports", "The Environment"],
 };
-
 const user2: User = {
   id: 2,
   email: "user2@test.com",
@@ -35,14 +33,13 @@ const user2: User = {
 };
 
 /**
- * Part 3: Change the types in the following code from 'any' to a more speficic type
+ * Part 3: Change the types in the following code from 'any' to something more specific.
  *
  * Hint: Search for 'TS optional values' or 'TS optional object values'
  * Hint: Search for 'TS literal types'
  */
 
 export type Post = any;
-
 export const post1: Post = {
   id: 1,
   userId: 1,
@@ -60,7 +57,7 @@ const post2: Post = {
 };
 
 /**
- * Part 4: Change the types in the following code from 'any' to a more specific types
+ * Part 4: Change the types in the following code from 'any' to something more specific.
  *
  * Hint: Search for 'TS typing empty array'
  * Hint: Search for 'TS promise types'
@@ -85,67 +82,60 @@ const postByNumberId = getPostById(1);
 const postByStringId = getPostById("1");
 
 /**
- * Part 5: Change the types in the following code from 'any' to a more specific type. Assume data is always a JS object
+ * Part 5: Change the types in the following code from 'any' to something more specific.
+ *
+ * Assume the 'processAPIRequest' can handle any data as long as it's an object.
  *
  * Hint: Search for 'TS unknown objects'
  */
 
 type UnknownData = any;
 
-const processRequest = (data: UnknownData): any => {
-  return "Hey";
+const processAPIRequest = (data: UnknownData): any => {
+  return `API request processed with data: ${JSON.stringify(data)}`;
 };
+
+processAPIRequest({
+  foo: "foo",
+  bar: "bar",
+});
 
 /**
- * Part 6: Change the types in the following code from 'any' to a more specific type
+ * Part 6: Change the types in the following code from 'any' to something more specific.
  *
- * Hint: Search for 'TS partially known object'
- */
-
-type DataWithExpectedValue = any;
-
-const processRequestWithUserId = (data: DataWithExpectedValue): any => {
-  if (!data.userId) {
-    return "Hey";
-  }
-  return `Hey ${data.userId}`;
-};
-
-const maybeProcessRequestWithUserId = (data: DataWithExpectedValue): any => {
-  if (!data.userId) {
-    console.log("Expected a User ID");
-    return;
-  }
-  return `Hey ${data.userId}`;
-};
-
-/**
- * Part 7: Change the types in the following code from 'any' to a more specific type
- *
- * Hint: Search for 'TS ReturnType utility'... Maybe also have a look at the other utilities available in TS?
+ * Hint: Search for 'TS ReturnType utility'. Maybe also have a wee look at the other utilities available while you're there...
  * Hint: Search for 'TS typeof'
  */
 
-const pretendImADatabaseOfUsers = [
+const pretendImADatabaseTableOfUsers = [
   { userId: "abc123", followers: [], blockedUsers: [] },
   { userId: "def456", followers: [], blockedUsers: ["abc123"] },
 ];
 
-const followUser = (followerId: any, toFollowId: any) => {
-  const toFollowUser = pretendImADatabaseOfUsers.find(
-    (user) => user.userId === toFollowId
+/**
+ * Note that we haven't specified a return type for this function.
+ * The TS compiler is smart - it can infer types in a lot of cases so we don't have to manually define them...
+ *
+ * Hovering over the 'followUser' variable below should show:
+ *   const followUser: (followerId: any, userToFollowId: any) => "FAIL/USER_BLOCKED" | "SUCCESS/USER_FOLLOWED"
+ * The TS compiler already figured out that this function can only ever return "FAIL/USER_BLOCKED" or "SUCCESS/USER_FOLLOWED"
+ */
+const followUser = (followerId: any, userToFollowId: any) => {
+  const toFollowUser = pretendImADatabaseTableOfUsers.find(
+    (user) => user.userId === userToFollowId
   );
   if (toFollowUser?.blockedUsers.includes(followerId)) {
     return "FAIL/USER_BLOCKED";
   }
 
-  // This is where we'd normally update the user's followers and then save the user back into the DB (skipping since not important here)
+  // This is where we'd normally update the user's followers in the database (skipping here since the implementation isn't important)
 
   return "SUCCESS/USER_FOLLOWED";
 };
 
-followUser("abc123", "def456");
+followUser("abc123", "def456"); // user with ID 'abc123' wants to follow user with ID 'def456'
 
 type FollowUserPossibleOutcomes = any; // Hovering over 'FollowUserPossibleOutcomes' should show "FAIL/USER_BLOCKED" | "SUCCESS/USER_FOLLOWED"
 
+// Export to make TS happy
 export {};
--- problems/set2.ts
+++ problems/set2.ts
@@ -1,24 +1,23 @@
 import { Post, User, post1, user1 } from "./set1";
 
 /**
- * Part 1: Write a utility type that converts a specific entity type into an array of that entity type
+ * Part 1: Write a utility type that takes an input type and returns a union of that type with null and undefined, i.e. SomeType ---> SomeType | null | undefined
  *
  * Hint: Search for 'TS generics'
  * Hint: Search for 'TS custom utility types with generics'
  */
 
-type EntityArray<...> = ...;
+type Nullish<...> = ...;
 
-type ArrayOfUsers = EntityArray<User>; // Hovering over 'ArrayOfUsers' should show User[], i.e. an array where all values inside are Users
-type ArrayOfPosts = EntityArray<Post>; // Hovering over 'ArrayOfPosts' should show Post[], i.e. an array where all values inside are Posts
+type NullishUser = Nullish<User>; // Hovering over 'NullishUser' should show 'User | null | undefined'
+type NullishPost = Nullish<Post>; // Hovering over 'NullishPost' should show 'Post | null | undefined'
 
 /**
  * Part 2: Change the types in the following code from 'any' to a more specific object type, utilising generics where useful.
  * 
- * The function 'modifyEntity' takes two arguments:
- * 
- * entity: either a User or a Post
- * modifyFn: a function which takes an entity, adds some data and returns the modified entity
+ * The function 'modifyEntity' should take two arguments:
+ *   entity: either a User or a Post
+ *   modifyFn: a function which takes an entity, adds some data and returns the modified entity
  *
  * Hint: Search for 'TS as const'
  */
@@ -49,7 +48,7 @@ const childFriendlyPost = modifyEntity(post1, markPostAsChildFriendly);
 type ChildFriendlyPost = typeof childFriendlyPost; // Hovering over 'ChildFriendlyPost' should show a Post type containing 'isChildFriendly: true'
 
 /**
- * Part 3: Write a utility type, using generics, mapped types and conditional types, which transforms string array type into a union of a string array and a string, i.e. string[] ---> string[] | string.
+ * Part 3: Write a utility type which takes an object as an input and transforms any string array types in the object into a union of a string array and a string, i.e. string[] ---> string[] | string.
  *
  * Hint: Search for 'TS mapped types'
  * Hint: Search for 'TS conditional types'
@@ -59,4 +58,5 @@ type TransformStringArraysInObjectToIncludeStringsToo<...> = ...;
 
 type UserWhereFavouriteTopicsCanBeAString = TransformStringArraysInObjectToIncludeStringsToo<User>; // Hovering over 'UserWhereFavouriteTopicsCanBeAString' should show a User type where 'favouriteTopics: string[] | string'
 
+// Export to make TS happy
 export { };
