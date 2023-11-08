9ebd289b988ae6684d958450337fbab45e0d6ea7
Craig McCallum <craig@pensionbee.com>
2023-11-08 11:08:36 +0000

    Update ts problems

--- README.md
+++ README.md
@@ -11,9 +11,5 @@ Feel free to check these out before or after completing 'The Practical Bit' belo
 
 ## The Practical Bit
 
-First, run `nvm use` then `npm install` to make sure you have the necessary typescript support to work through this section.
-
-In the **problems/** directory you'll find two problem sets...
-
-- Set 1: The `If you can do this then you have a great foundation for working in a Typescript codebase` set. There's a lot to Typescript and you probably won't need most of it 95% of the time. The basics are where it's at - seriously! Check out the hints in each exercise which should guide you towards a solution.
-- Set 2: This focuses on generics and a couple of other more advanced TS concepts, which are very useful for building libraries or shared utilities. If you're up for the challenge, this stuff probably isn't as difficult as you might initially think, and if you can get through it, you're on your way to becoming a real TS wizard. Remember to check out the hints which should help guide you towards a solution. However, if you're just like 'NOPE, I CAN'T DEAL WITH THIS RIGHT NOW!!' then no worries... Make yourself a hot beverage and ignore this stuff until you find yourself in a situation where the basics just don't quite cut it.
+1. First, run `nvm use` then `npm install` to make sure you have the necessary typescript support to work through the problem set
+2. Tackle the 6 problems (plus the bonus problem if you're feeling up to it) in **problems.ts**
---
+++ problems.ts
@@ -0,0 +1,181 @@
+/**
+ * PROBLEM 1: Change the types in the following code from 'any' to something more specific.
+ *
+ * Hint: Search for 'TS basic types'
+ */
+
+const id: any = 1;
+const email: any = "user@test.com";
+const isVerified: any = true;
+const favouriteTopics: any = ["World Politics", "Sports", "The Environment"];
+
+/**
+ * PROBLEM 2: Change the types in the following code from 'any' to something more specific.
+ *
+ * Hint: Search for 'TS object types'
+ * Hint: Search for 'TS unions'
+ */
+
+type User = any;
+
+const user1: User = {
+  id: 1,
+  email: "user1@test.com",
+  backupEmail: null,
+  isVerified: true,
+  favouriteTopics: ["World Politics", "Sports", "The Environment"],
+};
+const user2: User = {
+  id: 2,
+  email: "user2@test.com",
+  backupEmail: "user2Backup@test.com",
+  isVerified: false,
+  favouriteTopics: [],
+};
+
+/**
+ * PROBLEM 3: Change the types in the following code from 'any' to something more specific.
+ *
+ * Hint: Search for 'TS optional values' or 'TS optional object values'
+ * Hint: Search for 'TS literal types'
+ */
+
+type Post = any;
+const post1: Post = {
+  id: 1,
+  userId: 1,
+  status: "DRAFT", // Should only ever be "DRAFT" or "PUBLISHED"
+  title: "Going on holiday with Bae",
+  content: "It's our 62nd wedding anniversary and we're off to Benidorm",
+};
+
+const post2: Post = {
+  id: 2,
+  userId: 1,
+  status: "PUBLISHED", // Should only ever be "DRAFT" or "PUBLISHED"
+  title: "Just ate a chicken - was yum",
+  imageUrl: "https://my-pics.com/the-chicken-i-just-ate.png",
+};
+
+/**
+ * PROBLEM 4: Change the types in the following code from 'any' to something more specific.
+ *
+ * Hint: Search for 'TS typing empty array'
+ * Hint: Search for 'TS promise types'
+ */
+
+const allPosts = [post1, post2];
+
+function getPostsByUserId(userId: any): any {
+  return allPosts.filter((post) => post.userId === userId);
+}
+
+// Example usage
+const userPosts = getPostsByUserId(1);
+
+async function getPostById (id: any): any {
+  if (typeof id === "string") {
+    return allPosts.find((post) => post.id === parseInt(id));
+  }
+  return allPosts.find((post) => post.id === id);
+};
+
+// Example usage
+const postByNumberId = getPostById(1);
+const postByStringId = getPostById("1");
+
+/**
+ * PROBLEM 5: Change the types in the following code from 'any' to something more specific.
+ *
+ * Assume 'processAPIRequest' can handle any data as long as it's an object.
+ *
+ * Hint: Search for 'TS unknown objects'
+ */
+
+type UnknownData = any;
+
+function processAPIRequest (data: UnknownData): any {
+  return `API request processed with data: ${JSON.stringify(data)}`;
+};
+
+// Example usage
+processAPIRequest({
+  foo: "foo",
+  bar: "bar",
+});
+
+/**
+ * PROBLEM 6: Change the types in the following code from 'any' to something more specific.
+ *
+ * Hint: Search for 'TS ReturnType utility' (Note that you can see all the built-in utility types here: https://www.typescriptlang.org/docs/handbook/utility-types.html)
+ * Hint: Search for 'TS typeof'
+ */
+
+/**
+ * Note that we haven't specified a return type for the 'followUser' function below.
+ * The TS compiler is smart - it can actually infer types in a lot of cases so we don't have to manually define them.
+ *
+ * Hovering over the 'followUser' function below should show the following:
+ *   function followUser(followerId: any, userToFollowId: any): "FAIL/USER_BLOCKED" | "SUCCESS/USER_FOLLOWED"
+ * 
+ * Here the TS compiler figured out that this function can only ever return "FAIL/USER_BLOCKED" or "SUCCESS/USER_FOLLOWED"
+ */
+
+type FollowUserPossibleOutcomes = any; // Hovering over 'FollowUserPossibleOutcomes' should show "FAIL/USER_BLOCKED" | "SUCCESS/USER_FOLLOWED"
+
+const usersData = [
+  { userId: "user-1", followers: [], blockedUsers: [] },
+  { userId: "user-2", followers: [], blockedUsers: ["user-1"] },
+];
+
+function followUser (followerId: any, userToFollowId: any) {
+  const toFollowUser = usersData.find(
+    (user) => user.userId === userToFollowId
+  );
+  if (toFollowUser?.blockedUsers.includes(followerId)) {
+    return "FAIL/USER_BLOCKED";
+  }
+
+  // This is where we'd normally update the user's followers and persist the change (skipping here since the implementation isn't important)
+
+  return "SUCCESS/USER_FOLLOWED";
+};
+
+// Example usage
+followUser("user-1", "user-2"); // user with ID 'user-1' wants to follow user with ID 'user-2'
+
+/**
+ * BONUS PROBLEM
+ * 
+ * -----------------------------------------------------------------------------------------------------
+ * 
+ * This part introduces 'generics', which goes beyond Typescript basics...
+ * 
+ * Generics can take a little time to wrap your head around but they're an extremely powerful tool for
+ * building libraries or shared utilities where you don't know what specific types are going to be used
+ * ahead of time.
+ * 
+ * You won't need to write anything with generics in the rest of the workshop but some
+ * utility functions we'll use in later sections will use generics under the hood, so this is here
+ * for completeness in case you're interested in exploring those functions more when you reach them.
+ * 
+ * -----------------------------------------------------------------------------------------------------
+ * 
+ * Write a utility type that takes any object type and returns a union of that type with null and undefined, i.e. SomeType ---> SomeType | null | undefined
+ *
+ * Hint: Search for 'TS generics'
+ * Hint: Search for 'TS custom utility types with generics'
+ */
+
+type Nullish<...> = ...;
+
+// Example usage
+type NullishUser = Nullish<User>; // Hovering over 'NullishUser' should show 'User | null | undefined'
+type NullishPost = Nullish<Post>; // Hovering over 'NullishPost' should show 'Post | null | undefined'
+
+type ErrorTest1 = Nullish<1>; // This should be an error because 1 is not an object
+type ErrorTest2 = Nullish<'test'>; // This should be an error because 'test' is not an object
+type ErrorTest3 = Nullish<[5, 6, 7]>; // This should be an error because [5, 6, 7] is not an object
+
+// Export to make TS happy
+export { };
